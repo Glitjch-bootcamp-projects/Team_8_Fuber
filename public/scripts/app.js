dbaddbb30dda468db17aa04c377688deef87c2a8
JackyLin1 <jlin9702@gmail.com>
2022-02-14 07:51:55 +0900

    test

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,13 @@
 // Client facing scripts here
+
+$(document).ready(function() {
+  //toggles schedule on main page
+  $('.Schedule').click(function() {
+    $('#date').slideToggle();
+    $('#time').slideToggle();
+  });
+});
+
 $(()=> {
   console.log("client.js touched");
 
@@ -29,13 +38,8 @@ $(()=> {
       // store this data somewhere
       const serializeData = $(".addressBar").serialize();
       console.log('serializedata:    ', serializeData);
-
+      
     })
     // .then(() => generateRestaurants)
-$(document).ready(function() {
-  //toggles schedule on main page
-  $('.Schedule').click(function() {
-    $('#date').slideToggle();
-    $('#time').slideToggle();
+
   });
-});
