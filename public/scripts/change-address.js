87ddd338bb1dad59128f1758862760d721326b2e
glitjch <t.jamesphan@gmail.com>
2022-02-16 22:52:43 +0000

    added script and linked to ejs files for changing address button

---
+++ public/scripts/change-address.js
@@ -0,0 +1,8 @@
+$(() => {
+// when user clicks on change button log it
+$(".addressBtn").hover(()=>{
+  console.log("change address");
+})
+// when user clicks on button, text on address button updates
+
+});
