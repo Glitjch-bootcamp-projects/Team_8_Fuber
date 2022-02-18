54d59f75a3c8599f95e71f0d1cbec2527c025c7b
JackyLin1 <jlin9702@gmail.com>
2022-02-18 10:35:14 +0900

    app.js refactored, function: adds dynamic text for address button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,49 +3,62 @@
 $(() => {
   console.log("client.js touched");
   const local = "http://localhost:8080";
+
+  //on click add addressBar value to session storage. Also remove address saved in session on click.
+  $(`.addressBtn`).click(function(){
+    sessionStorage.removeItem(`address`);
+    window.sessionStorage.setItem(`address`, $(`.addressBar`).val())
+  });
+
+  //retrieve value in session storage and add to .ejs for button when page is loaded
+  $(`restaurants.ejs`).ready(() =>{
+    $(".header-address-text").text(window.sessionStorage.getItem(`address`))
+  });
   
+  //creates a small window to change address
   $('.changeAddressBtn').click(function() {
     $("#modal").css("transform", "scale(1)");
     $("#overlay").css("opacity", "1");
   })
 
+  //close button for the small window
   $('.closeBtn').click(function() {
     $("#modal").css("transform", "scale(0)");
     $("#overlay").css("opacity", "0");
   })
 
-  // Jacky
   //toggles schedule on main page
   $('.Schedule').click(function () {
     $('#date').slideToggle();
     $('#time').slideToggle();
   });
 
+  //filters based on price
   $('#p1').click(function () {
     $('.1').show();
     $(".2").hide();
-    $(".3").css("display", "none");
-    $(".4").css("display", "none");
+    $(".3").hide();
+    $(".4").hide();
   })
 
   $('#p2').click(function () {
-    $('.1').css("display", "none");
+    $('.1').hide();
     $('.2').show();
-    $('.3').css("display", "none");
-    $('.4').css("display", "none");
+    $('.3').hide();
+    $('.4').hide();
   })
 
   $('#p3').click(function () {
-    $('.1').css("display", "none");
-    $('.2').css("display", "none");
+    $('.1').hide();
+    $('.2').hide();
     $('.3').show();
-    $('.4').css("display", "none");
+    $('.4').hide();
   })
 
   $('#p4').click(function () {
-    $('.1').css("display", "none");
-    $('.2').css("display", "none");
-    $('.3').css("display", "none");
+    $('.1').hide();
+    $('.2').hide();
+    $('.3').hide();
     $('.4').show();
   })
 
